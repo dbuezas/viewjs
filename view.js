93e84e5aeb2886a839ef16db52ab4925c71e65e3
Andreas Tietz <andreas.tietz@gmail.com>
2014-02-15 03:40:50 +0100

    Improved and adapted API documentation to yuidoc format.

--- view.js
+++ view.js
@@ -1,9 +1,43 @@
 /**
- * ViewJS 0.2.1
+ * viewjs 0.2.1
  * Copyright (c) 2014, Andreas Tietz
  * Released under the MIT license
  */
 
+/**
+ *  #ViewJS
+ *    - JavaScript utility/library that facilitates development of large scale client-side web applications with rich user interfaces
+ *    - introduces a structural/architectural abstraction layer on top of common client-side web technology (HTML/CSS/JS)
+ *    - inspired by and built around concepts found in native UI development
+ *
+ *  ##Concept
+ *
+ *  Examination of a view from different perspectives & definition of terminology:
+ *
+ *  ### View Component (architectural point of view)
+ *    - a reusable component being instantiated at runtime
+ *    - consists of a view controller and a bundle
+ *    - behavior and styling may be extended/overridden/customized by inheritance/cascading
+ *
+ *  ### View Controller (runtime/behavior point of view)
+ *    - Constructor and prototype object providing common behavior across all instances of a view component
+ *
+ *  ### View Bundle (resource/graphical representation point of view)
+ *    - collection of resources providing means for a common graphical representation across all instances of a view component
+ *    - e.g. HTML markup, CSS stylesheet, media resources (e.g. images/audio/video)
+ *
+ *  ### View (general/runtime/instance point of view)
+ *    - concrete instance of a view component
+ *    - self-contained/self-managed piece of UI, most like a widget
+ *    - scoping: managed DOM subtree that has scoped styling and behavior
+ *    - composition: view instances can be in a parent/child relationship meaning that a view may have one or more other views nested inside of itself. This results in a tree structure, the "View Hierarchy"
+ *    - lifecycle: a view has a lifecycle (construction/instantiation, loading, [showing, hiding, doing stuff], unloading, deallocation)
+ *    - memory management
+ *
+ *  @author Andreas Tietz
+ *  @module ViewJS
+ *  @requires RequireJS, jQuery
+ */
 (function (factory) {
     // Define this module and it's dependencies via AMD if posssible, otherwise use the global scope:
     if (typeof define === "function" && define.amd) {
@@ -13,8 +47,11 @@
     }
 }(function ($) {
 
-    /*
-     *  Constructor
+    /**
+     *  Base implementation of a view controller providing scoped and configurable behavior across all instances of a view component.
+     *
+     *  @class View
+     *  @constructor
      */
     function View(config) {
         var self = this;
@@ -28,40 +65,37 @@
         // Lazily initialize the config hash:
         config = config || {};
 
-        // Public (properties):
-
-        // Unique name for the view to be distinguished from other views at the same hierarchy level:
+        // Initially assign properties from config:
         self.name = config.name;
-
-        // HTML & CSS module/filenames:
         self.bundle = config.bundle;
-
-        // Determines whether this view is the first view in a view hierarchy (has no parent view):
         self.isRootView = config.isRootView;
-
-        // Parent or "owner" of this view:
         self.parentView = config.parentView;
-
-        // Child views "owned" by this view:
         self.childViews = config.childViews;
-
-        // Subscription for viewDidLoad events:
         self.onViewDidLoad = config.onViewDidLoad;
-
-        // Subscription for viewWillUnload events:
         self.onViewWillUnload = config.onViewWillUnload;
-
-        // Private:
-
-        self._viewElement; // loaded view root DOM node (replacement of the original container element)
-        self._containerElement; // backup copy of the original container element for restoring upon unloading
     }
 
     /*
      *  Inheritance
      */
 
-    // Helper function making prototypal inheritance from Views syntactically cleaner:
+    /**
+     *  Helper function that facilitates prototypal inheritance from Views.
+     *
+     *  Note: Assign a name to the passed in constructor function in order for it to be correctly printed out in console logs.
+     *
+     *  @method extend
+     *  @param {Mixed} [extension] Prototype attributes and methods being added to the view prototype to be inherited from.
+     *  @return {Function} Constructor of the extended/inherited view.
+     *  @example
+     *      var MyView = View.prototype.extend({
+     *          constructor: function MyView (config) {..},
+     *          viewDidLoad: function () {..},
+     *          myNewFunction: function () {..}
+     *          myNewAttribute: ..
+     *          ...
+     *      };
+     */
     View.prototype.extend = function (extension) {
         var self = this;
 
@@ -95,24 +129,50 @@
      *  Lifecycle inheritance interface/notification hooks
      */
 
+    /**
+     *  Function being called on the view itself when the "view did load" event occurs.
+     *
+     *  Inherited views may override and implement this function.
+     *
+     *  @method viewDidLoad
+     */
     View.prototype.viewDidLoad = function () {
         // optionally override and implement in inherited view
     };
 
+    /**
+     *  Function being called on the view itself when the "view will unload" event occurs.
+     *
+     *  Inherited views may override and implement this function.
+     *
+     *  @method viewWillUnload
+     */
     View.prototype.viewWillUnload = function () {
         // optionally override and implement in inherited view
     };
 
     /*
-     *  Accessors
+     *  Public attributes, properties and accessors
      */
 
-    // Helper function for defining properties:
+    /**
+     *  Helper function for defining properties.
+     *
+     *  @method defineProperty
+     *  @param {String} [name] Name of the property.
+     *  @param {Mixed} [accessors] Object defining set and get functions.
+     */
     View.prototype.defineProperty = function (name, accessors) {
         Object.defineProperty(this.constructor.prototype, name, accessors);
     };
 
-    // name:
+    /**
+     *  Unique name for the view to be distinguished from other views at the same hierarchy level.
+     *
+     *  @property name
+     *  @type String
+     *  @default undefined
+     */
     View.prototype.defineProperty("name", {
         get: function () { return this._name; },
         set: function (name) {
@@ -121,7 +181,13 @@
         }
     });
 
-    // bundle:
+    /**
+     *  HTML & CSS module/filenames.
+     *
+     *  @property bundle
+     *  @type String
+     *  @default undefined
+     */
     View.prototype.defineProperty("bundle", {
         get: function () { return this._bundle; },
         set: function (bundle) {
@@ -130,7 +196,13 @@
         }
     });
 
-    // isRootView:
+    /**
+     *  Determines whether the view is the first view in a view hierarchy and therefore has no parent view.
+     *
+     *  @property isRootView
+     *  @type Boolean
+     *  @default false
+     */
     View.prototype.defineProperty("isRootView", {
         get: function () { return this._isRootView; },
         set: function (isRootView) {
@@ -139,7 +211,13 @@
         }
     });
 
-    // parentView:
+    /**
+     *  Parent view up the view hierarchy that "owns" the view.
+     *
+     *  @property parentView
+     *  @type View
+     *  @default undefined
+     */
     View.prototype.defineProperty("parentView", {
         get: function () { return this._parentView; },
         set: function (parentView) {
@@ -153,7 +231,13 @@
         }
     });
 
-    // childViews:
+    /**
+     *  Direct child views down the view hierarchy "owned" by the view.
+     *
+     *  @property childViews
+     *  @type Array of Views
+     *  @default []
+     */
     View.prototype.defineProperty("childViews", {
         get: function () { return this._childViews || []; },
         set: function (childViews) {
@@ -175,7 +259,16 @@
         }
     });
 
-    // onViewDidLoad:
+    /**
+     *  Subscription for "view did load" events.
+     *
+     *  @property onViewDidLoad
+     *  @type Mixed
+     *  @default {
+     *               subscribe: Function
+     *               unsubscribe: Function
+     *           }
+     */
     View.prototype.defineProperty("onViewDidLoad", {
         get: function () {
             this._onViewDidLoadCallbacks = this._onViewDidLoadCallbacks || $.Callbacks("unique memory");
@@ -190,7 +283,16 @@
         }
     });
 
-    // onViewWillUnload:
+    /**
+     *  Subscription for "view will unload" events.
+     *
+     *  @property onViewWillUnload
+     *  @type Mixed
+     *  @default {
+     *               subscribe: Function
+     *               unsubscribe: Function
+     *           }
+     */
     View.prototype.defineProperty("onViewWillUnload", {
         get: function () {
             this._onViewWillUnloadCallbacks = this._onViewWillUnloadCallbacks || $.Callbacks("unique memory");
@@ -205,9 +307,39 @@
         }
     });
 
-    // isLoaded determines whether the view's DOM is currently loaded (true if _viewElement and _containerElement exist):
+    /*
+     *  Private attributes
+     */
+
+    /**
+     *  Backup copy of the original container element to be restored when unloading.
+     *
+     *  @property _containerElement
+     *  @private
+     *  @type jQuery Element
+     */
+    View.prototype._containerElement;
+
+    /**
+     *  Loaded view root DOM node (replacement of the original container element).
+     *
+     *  @property _viewElement
+     *  @private
+     *  @type jQuery Element
+     */
+    View.prototype._viewElement;
+
+    /**
+     *  Determines whether the view's DOM is currently loaded.
+     *
+     *  @property isLoaded
+     *  @readOnly
+     *  @type Boolean
+     *  @default false
+     */
     View.prototype.defineProperty("isLoaded", {
         get: function () {
+            // true if _viewElement and _containerElement exist:
             return (this._viewElement && this._viewElement.length > 0 && this._containerElement && this._containerElement.length > 0);
         }
     });
@@ -216,11 +348,17 @@
      *  Markup/DOM management
      */
 
-    // Performs a jQuery selector scoped to the root element of the loaded view:
-    // - returns the root element of the loaded view if invoked without parameters
-    // - returns undefined if the view is not loaded
-    // TODO: don't select deeper than the parent view (ignore inner DOM of child views)
+    /**
+     *  Performs a jQuery selector scoped to the root element of a loaded view.
+     *
+     *  @method $
+     *  @param {String} [selector] jQuery selector being scoped to the root element.
+     *  @return {jQuery Element} Root element of a loaded view if invoked without a parameter,
+     *                           scoped jQuery selection result if invoked with a selector
+     *                           or undefined if the view is not loaded.
+     */
     View.prototype.$ = function (selector) {
+        // TODO: don't select deeper than the parent view (ignore inner DOM of child views)
         var self = this;
         if (self.isLoaded) {
             return selector ? self._viewElement.find(selector) : self._viewElement;
@@ -233,8 +371,12 @@
         }
     };
 
-    // Loads markup from the HTML file specified by self._bundle into the container element
-    // identified by self._name, also loads associated scoped CSS styles from the bundle:
+    /**
+     *  Loads markup and styles from HTML and CSS files specified by the bundle property into the container element identified by the name property.
+     *
+     *  @method load
+     *  @return {jQuery Deferred Promise} Promise for the "view did load" state.
+     */
     View.prototype.load = function () {
         var self = this;
 
@@ -327,7 +469,12 @@
         return viewDidLoad.promise();
     };
 
-    // Unloads the DOM by emptying the container:
+    /**
+     *  Unloads the DOM by emptying the container.
+     *
+     *  @method unload
+     *  @return {jQuery Deferred Promise} Promise for the "view will unload" state.
+     */
     View.prototype.unload = function () {
         var self = this;
 
@@ -362,8 +509,14 @@
      *  Debugging
      */
 
-    // Returns a string representation of the view:
-    // Format: "<controller/constructor>/<bundle> @[<instance/container/name>]"
+    /**
+     *  Provides a human readable string representation of a view.
+     *
+     *  @method toString
+     *  @return {String} String representation of the view.
+     *  @example
+     *      Format: "<controller/constructor name>/<bundle name> @[<instance/container name>]"
+     */
     View.prototype.toString = function () {
         var self = this;
         var str = self.constructor.name + (self.constructor.name === self.bundle ? "" : "/" + self.bundle);
@@ -371,8 +524,14 @@
         return str;
     };
 
-    // Traverses the view hierarchy recursively starting with self and it's child views
-    // and invokes a function for each view passing the view and the recursion level:
+    /**
+     *  Recursively traverses down the child view hierarchy of a view while invoking
+     *  a function for each view passing it the view and the recursion level.
+     *
+     *  @method traverse
+     *  @param {Function} fn Function being invoked for each view and being passed the view object and the recursion level.
+     *  @param {Number} level Indicates the current level of recursion.
+     */
     View.prototype.traverse = function (fn, level) {
         var self = this;
         if (level === undefined) level = 0;
@@ -382,7 +541,12 @@
         });
     };
 
-    // Returns the current state of the view hierarchy as a humman readable string:
+    /**
+     *  Provides a human readable string representation of the current state of the child view hierarchy.
+     *
+     *  @method getViewHierarchyHumanReadable
+     *  @return {String} String representation of the child view hierarchy.
+     */
     View.prototype.getViewHierarchyHumanReadable = function () {
         var self = this;
         var hierarchyOutput = "";
